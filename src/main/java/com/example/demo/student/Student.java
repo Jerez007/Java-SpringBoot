699a2afc7e797c970f2846ba080b477e9fbdc906
Jerez <jerezsolis@laptop.local>
2023-05-29 16:53:40 -0700

    initialize files

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,10 @@
+package com.example.demo.student;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+}
