a29bd0a9ab0d254716745c0295f9500ca3d5011b
JJ <jerez.solis@gmail.com>
2023-06-01 16:39:16 -0700

    added validation to check if email already exists in the database

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,11 @@ public class StudentController {
     // This is the API layer
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    // The API to take payload and store it in the database
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 }
--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // This acts as the database layer. It is responsible for data access.
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // Custom function to write user by email
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
